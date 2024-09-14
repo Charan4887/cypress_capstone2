dde9c09d58c148cac2d655cc1813f4818fecd08c
Jaisa2001 <jaisaicharan2001@gmail.com>
2024-09-14 18:17:23 +0530

    solved

--- cypress/e2e/CustomCommands.cy.js
+++ cypress/e2e/CustomCommands.cy.js
@@ -1,13 +1,7 @@
 describe('Custom commands', () => {
-
-
+    
     it('using custom command for  login', () => {
-
     cy.login('demo-customer@x-cart.com','customer')
-
     cy.get('.name').should('have.text','(Mr. John Smith)')
-
-
     })
-
 })
--- cypress/e2e/EdgeCases.cy.js
+++ cypress/e2e/EdgeCases.cy.js
@@ -1,25 +1,15 @@
 describe('Edge cases', () => {
 
     it('login with invalid credintials', () => {
-
       cy.visit('https://demo.x-cart.com/demo/login.php')
-  
       cy.get('#username').clear().type('demo-custome@x-cart.com')
-
       cy.get('#password').clear().type('custoer')
-
       cy.get('.button-left').click()
-
       cy.get('#top-message > .box').should('contain.text','incorrect Email or Password')
-
       //invalid product searching
-
       cy.get("div[class='line3'] input[placeholder='Enter keyword...']").type('animal')
-
       cy.get("div[class='line3'] button[type='submit']").click()
-
       cy.get('#top-message > .box').should('contain.text','Try again with different criteria')
-
     })
 
 })
